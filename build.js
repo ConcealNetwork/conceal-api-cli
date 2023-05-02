f508968ed580c00ed6d2f9450456cd6ca7e69d6b
Taegus <taegus.cromis@gmail.com>
2023-05-02 13:10:00 +0200

    added automatic compression to build script

--- build.js
+++ build.js
@@ -1,13 +1,25 @@
+import compress from "compressing";
 import caxa from "caxa";
-import Os from 'os';
+import os from 'os';
+import fs from 'fs';
 
 (async () => {
+  fs.rmSync("./bin", { recursive: true, force: true });
+  fs.mkdirSync("./bin");
+  fs.copyFileSync("./config.json", "./bin/config.json");
+
   await caxa({
     input: ".",
-    output: Os.platform() === "win32" ? "conceal-cli.exe" : "conceal-cli",
+    output: os.platform() === "win32" ? "./bin/conceal-cli.exe" : "./bin/conceal-cli",
     command: [
       "{{caxa}}/node_modules/.bin/node",
       "{{caxa}}/index.js"
     ],
   });
+
+  if (os.platform() === "win32") {
+    await compress.zip.compressDir("./bin", "./conceal-cli.zip");
+  } else {
+    await compress.tgz.compressDir("./bin", "./conceal-cli.tar.gz");
+  }
 })();
